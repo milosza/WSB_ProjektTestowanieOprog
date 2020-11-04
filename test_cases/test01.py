ced3e7bd1999dc6534fe3dd7d0c97001b5aea2b2
Milosz A <milosza@users.noreply.github.com>
2020-11-04 16:32:49 +0100

    update

--- test_cases/test01.py
+++ test_cases/test01.py
@@ -5,23 +5,28 @@ from page_objects.login_page import LoginPage
 def test_logger(logger):
     logger.info("Logowanie z poziomu testów")
 
+
 def test_prerequisites_check_registration_page_components(browser):
     registration_page = RegistrationPage(browser)
     assert registration_page.validate_logo_is_visible()
     assert registration_page.validate_zalogujSie_is_visible()
     assert registration_page.page_opened()
 
+
 def test_1_1_1_create_account_empty_fields_validation(browser):
     registration_page = RegistrationPage(browser)
     assert registration_page.empty_fields()
+
 
 def test_1_1_2_to_1_1_3_create_account_firstname_validation(browser):
     registration_page = RegistrationPage(browser)
     assert registration_page.firstname_check()
 
+
 def test_1_1_4_to_1_1_5_create_account_lastname_validation(browser):
     registration_page = RegistrationPage(browser)
     assert registration_page.lastname_check()
+
 
 def test_1_1_6_to_1_1_9_create_account_email_validation(browser):
     registration_page = RegistrationPage(browser)
